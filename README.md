e315536e475d802533a7fa4cd9fed0ffee286c7f
Diego Augusto Ramos <diego.ramos@adtsys.com.br>
2017-04-03 12:40:33 -0300

    thats it

---
+++ README.md
@@ -0,0 +1,8 @@
+# It's a simple alias with autocompletion for docker-compose command.
+
+## SETUP
+
+### Linux:
+```
+$ wget https://raw.githubusercontent.com/diegoadt/dco/master/linux/setup.sh -O /tmp/dco_setup.sh && chmod +x /tmp/dco_setup.sh && /bin/bash /tmp/dco_setup.sh && rm /tmp/dco_setup.sh
+```
---
+++ linux/setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+alias dco 2>/dev/null >/dev/null || echo "alias dco='docker-compose'" >>  ~/.bashrc
+
+sudo  wget https://raw.githubusercontent.com/diegoadt/dco/master/linux/dco_completion -O /etc/bash_completion.d/dco_completion
+
+echo "================================================================="
+echo "Reload your terminal and you will be ready to use 'dco' command."
